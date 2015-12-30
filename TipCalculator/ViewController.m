d7da21e0f861c7484bcc051be7d24fc6d57fa029
Renee <rtso2@illinois.edu>
2015-12-29 17:25:11 -0800

    Added segmented control to select percent tip

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *checkTotal;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipAmount;
 @property NSInteger percentTip;
 
 @end
@@ -20,6 +21,18 @@
     float check = [self.checkTotal.text floatValue];
     float tip = check * ((float)self.percentTip / 100.0);
     self.tipLabel.text = [NSString stringWithFormat:@"%.2f", tip];
+}
+- (IBAction)tipAmountChanged:(UISegmentedControl *)sender {
+    if (self.tipAmount.selectedSegmentIndex == 0) {
+        self.percentTip = 10;
+    } else if (self.tipAmount.selectedSegmentIndex == 1) {
+        self.percentTip = 15;
+    } else {
+        self.percentTip = 20;
+    }
+    float check = [self.checkTotal.text floatValue];
+    float tip = check * ((float)self.percentTip / 100.0);
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
 }
 
 - (void)viewDidLoad {
