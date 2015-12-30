4ffe516f6c2f7e9e5d941527640f6e3d6d3618c8
Renee <rtso2@illinois.edu>
2015-12-29 17:05:01 -0800

    Basic tip calculating functionality

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -8,15 +8,24 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *checkTotal;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property NSInteger percentTip;
 
 @end
 
 @implementation ViewController
+- (IBAction)textFieldChanged:(UITextField *)sender {
+    float check = [self.checkTotal.text floatValue];
+    float tip = check * ((float)self.percentTip / 100.0);
+    self.tipLabel.text = [NSString stringWithFormat:@"%.2f", tip];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.percentTip = 15;
 }
 
 - (void)didReceiveMemoryWarning {
